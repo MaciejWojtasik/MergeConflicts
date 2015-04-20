5a6eb335faef6c79e764fdda6954b009e12ebb39
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-20 16:09:00 +0200

    added maths

--- src/mergeconflicts/Main.java
+++ src/mergeconflicts/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Hello again!");
-        System.out.println("Hello again!");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
     }
 }
